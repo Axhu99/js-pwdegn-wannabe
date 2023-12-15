f327fbb858da3fd7dd1da91be0dea77bd26114d4
Axhu99 <xhuliodreni@icloud.com>
2023-12-15 18:27:25 +0100

    unito gli elementi

--- js/script.js
+++ js/script.js
@@ -6,9 +6,17 @@ const firstname = prompt('Qual\'è il tuo NOME?', 'Xhulio');
 /* 3 chiedo il cognome 
    4 lo salvo in una variabile */
 
-const secondname = prompt('Qual\'è il tuo COGNOME?', 'Dreni');
+const lastname = prompt('Qual\'è il tuo COGNOME?', 'Dreni');
 
 /* 5 generare un numero casuale da 0 a 100 */
 let num = Math.round(Math.random() * 100);
 
 console.log(num)
+
+/* 6. sommiamo/uniamo queste 3 stringe
+   7. salviamo in una variabile c */
+
+const message = `la tua password e': ${firstname}${lastname}${num}`;
+
+console.log(message);
+
