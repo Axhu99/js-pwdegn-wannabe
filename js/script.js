53cfda750fe5497004a0c027efee70bc4f467208
Axhu99 <xhuliodreni@icloud.com>
2023-12-15 18:21:23 +0100

    generatore di numeri

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ const firstname = prompt('Qual\'è il tuo NOME?', 'Xhulio');
    4 lo salvo in una variabile */
 
 const secondname = prompt('Qual\'è il tuo COGNOME?', 'Dreni');
+
+/* 5 generare un numero casuale da 0 a 100 */
+let num = Math.round(Math.random() * 100);
+
+console.log(num)
