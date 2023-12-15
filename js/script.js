f2482b7860b6982c587d5fb425757091f9ea8a70
Axhu99 <xhuliodreni@icloud.com>
2023-12-15 18:05:36 +0100

    first prompt

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 1 chiedo il nome 
+   2 lo salvo in una variabile */
+
+const firstname = prompt('Qual\'è il tuo NOME?', 'Xhulio');
+
+/* 3 chiedo il cognome 
+   4 lo salvo in una variabile */
+
+const secondname = prompt('Qual\'è il tuo COGNOME?', 'Dreni');
